4fbd7a1c421054286d621a13e0e0321ac9105f02
Alexis <practicante.desarrollogh@regency.com.co>
2025-07-14 15:52:36 -0500

    cambios realizados

--- programacion_models/admin.py
+++ programacion_models/admin.py
@@ -1,16 +1,9 @@
 from django.contrib import admin
-from .models import ModeloTurno, LetraTurno
+from .models import ModeloTurno
 from .forms import ModeloTurnoForm
-
-class LetraTurnoInline(admin.TabularInline):
-    model = LetraTurno
-    extra = 0
-    readonly_fields = ('fila', 'columna', 'valor')
-    can_delete = False
 
 @admin.register(ModeloTurno)
 class ModeloTurnoAdmin(admin.ModelAdmin):
     form = ModeloTurnoForm
     list_display = ('nombre', 'unidad_negocio', 'tipo')
     search_fields = ('nombre',)
-    inlines = [LetraTurnoInline]
--- programacion_models/forms.py
+++ programacion_models/forms.py
@@ -4,15 +4,16 @@ from django.utils.safestring import mark_safe
 
 class MatrizLetrasWidget(forms.Widget):
     def render(self, name, value, attrs=None, renderer=None):
-        if not value:
-            return mark_safe("<p>La matriz se inicializará después de guardar y editar este modelo.</p>")
+        # Si value es None o no es una lista, inicializa una matriz 3x3 vacía
+        if not value or not isinstance(value, list):
+            value = [["" for _ in range(3)] for _ in range(3)]
         html = '<table style="border-collapse: collapse;">'
         for i, fila in enumerate(value):
             html += '<tr>'
             for j, letra in enumerate(fila):
                 html += (
                     f'<td style="border:1px solid #ccc;padding:2px;">'
-                    f'<input type="text" name="{name}_{i}_{j}" value="{letra or ""}" size="1" maxlength="1" style="text-align:center;" />'
+                    f'<input type="text" name="matriz_letras_{i}_{j}" value="{letra or ""}" size="1" maxlength="1" style="text-align:center;" />'
                     f'</td>'
                 )
             html += '</tr>'
@@ -26,7 +27,7 @@ class MatrizLetrasWidget(forms.Widget):
             fila = []
             j = 0
             while True:
-                key = f"{name}_{i}_{j}"
+                key = f"matriz_letras_{i}_{j}"
                 if key in data:
                     fila.append(data[key])
                     j += 1
@@ -39,12 +40,18 @@ class MatrizLetrasWidget(forms.Widget):
                 break
         return filas
 
+class MatrizLetrasField(forms.Field):
+    widget = MatrizLetrasWidget
+
+    def to_python(self, value):
+        return value
+
+    def validate(self, value):
+        super().validate(value)
+        # Aquí puedes agregar validaciones adicionales si quieres
+
 class ModeloTurnoForm(forms.ModelForm):
-    matriz_letras = forms.Field(
-        required=False,
-        widget=MatrizLetrasWidget,
-        label="Matriz de Letras"
-    )
+    matriz_letras = MatrizLetrasField(label="Matriz de Letras", required=False)
 
     class Meta:
         model = ModeloTurno
@@ -62,16 +69,21 @@ class ModeloTurnoForm(forms.ModelForm):
         else:
             self.initial['matriz_letras'] = [["" for _ in range(3)] for _ in range(3)]  # Por defecto 3x3
 
-    def save(self, commit=True):
-        instance = super().save(commit=False)
+    def clean_matriz_letras(self):
         matriz = self.cleaned_data.get('matriz_letras', [])
-        if commit:
-            instance.save()  # Asegura que tenga un ID
-            if matriz:
-                # Eliminar letras anteriores
-                instance.letras.all().delete()
-                for fila_idx, fila in enumerate(matriz):
-                    for col_idx, valor in enumerate(fila):
-                        if valor:
-                            instance.letras.create(fila=fila_idx, columna=col_idx, valor=valor)
+        if not any(any(cell for cell in fila) for fila in matriz):
+            raise forms.ValidationError("Debes ingresar al menos una letra en la matriz.")
+        return matriz
+
+    def save(self, commit=True):
+        instance = super().save(commit=commit)
+        matriz = self.cleaned_data.get('matriz_letras', [])
+        # Eliminar letras anteriores (si existen)
+        if instance.pk:
+            instance.letras.all().delete()
+            # Crear nuevas letras
+            for fila_idx, fila in enumerate(matriz):
+                for col_idx, valor in enumerate(fila):
+                    if valor:
+                        instance.letras.create(fila=fila_idx, columna=col_idx, valor=valor)
         return instance
